8d90513ad319009acbff1daefae80326a2999baa
Chi-En Wu <jason2506.mail@gmail.com>
2013-12-19 02:11:22 +0800

    Add `demo.py`

---
+++ demo.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import imfilter.preset as imf
+
+
+def main():
+    plt.figure()
+    img = plt.imread('lena.png')
+    for idx, name in enumerate(imf.__all__):
+        print 'Rendering filter: {}....'.format(name)
+
+        Filter = imf.__dict__.get(name)
+        f = Filter()
+        fimg = f(np.copy(img))
+
+        plt.subplot(3, 6, idx)
+        plt.title(name)
+        plt.axis('off')
+        plt.imshow(fimg)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
