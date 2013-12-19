30937f2a6174c39a7937cf04a0f8a0cf9d9eaa43
Chi-En Wu <jason2506.mail@gmail.com>
2013-12-19 12:51:48 +0800

    Remove `ABCMeta`

--- imfilter/base.py
+++ imfilter/base.py
@@ -1,23 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from abc import ABCMeta, abstractmethod
-
 __all__ = ('FilterBase',
            'BasicFilter')
 
 
 class FilterBase(object):
 
-    __metaclass__ = ABCMeta
-
-    @abstractmethod
     def __call__(self, img):
         return img
 
 
 class BasicFilter(FilterBase):
-
-    __metaclass__ = ABCMeta
 
     def __call__(self, img):
         img = self._process(img)
@@ -25,6 +18,5 @@ class BasicFilter(FilterBase):
         img[img < 0] = 0
         return img
 
-    @abstractmethod
     def _process(self, img):
         return img
